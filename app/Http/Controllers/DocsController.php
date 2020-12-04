f7dba4440b4cd7321cbb6982034fcdf1b52167d3
wngur6076 <wngur6076@naver.com>
2020-12-05 00:24:23 +0900

    엘릭서와 프런트엔드

--- app/Http/Controllers/DocsController.php
+++ app/Http/Controllers/DocsController.php
@@ -15,15 +15,15 @@ class DocsController extends Controller
 
     public function show($file = null)
     {
-        $index = markdown($this->docs->get());
-        $content = markdown($this->docs->get($file ?: 'installation.md'));
+        // $index = markdown($this->docs->get());
+        // $content = markdown($this->docs->get($file ?: 'installation.md'));
 
-        // $index = \Cache::remember('docs.index', 120, function () {          
-        //     return markdown($this->docs->get());
-        // });
-        // $content = \Cache::remember("docs.{$file}", 120, function () use ($file) {
-        //     return markdown($this->docs->get($file ?: 'installation.md'));
-        // });
+        $index = \Cache::remember('docs.index', 120, function () {          
+            return markdown($this->docs->get());
+        });
+        $content = \Cache::remember("docs.{$file}", 120, function () use ($file) {
+            return markdown($this->docs->get($file ?: 'installation.md'));
+        });
 
         return view('docs.show', compact('index', 'content'));
     }
